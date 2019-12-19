bcc90dae504e535a52d4ab2b18b3ba935c7090f7
Alexej Hamburg <alexej.hamburg1@gmail.com>
2019-12-20 00:35:01 +0100

    get base address, try to write to health

--- Cheat Engine Tutorial Hack.cpp
+++ Cheat Engine Tutorial Hack.cpp
@@ -1,20 +1,100 @@
-// Cheat Engine Tutorial Hack.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <vector>
+
+DWORD GetProcId(const std::wstring& processName)
+{
+	PROCESSENTRY32 processInfo;
+	processInfo.dwSize = sizeof(processInfo);
+
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (processesSnapshot == INVALID_HANDLE_VALUE)
+	{
+		return 0;
+	}
+
+	if (Process32First(processesSnapshot, &processInfo)) {
+		do
+		{
+			std::wcout << "Found process: " << processInfo.szExeFile << std::endl;
+			if (!processName.compare(processInfo.szExeFile))
+			{
+				CloseHandle(processesSnapshot);
+				return processInfo.th32ProcessID;
+			}
+		} while (Process32Next(processesSnapshot, &processInfo));
+	}
+
+	CloseHandle(processesSnapshot);
+	return 0;
+}
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName)
+{
+	uintptr_t modBaseAddr = 0;
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
+		MODULEENTRY32 modEntry;
+		modEntry.dwSize = sizeof(modEntry);
+		if (Module32First(hSnap, &modEntry))
+		{
+			do
+			{
+				std::wcout << "Found module: " << modEntry.szModule << std::endl;
+				if (!_wcsicmp(modEntry.szModule, modName))
+				{
+					modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					std::cout << "Found base address: " << "0x" << std::hex << modBaseAddr << std::endl;
+					break;
+				}
+			} while (Module32Next(hSnap, &modEntry));
+		}
+	}
+	CloseHandle(hSnap);
+	return modBaseAddr;
+}
+
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets)
+{
+	uintptr_t addr = ptr;
+	for (unsigned int i = 0; i < offsets.size(); ++i)
+	{
+		ReadProcessMemory(hProc, (BYTE*)(addr), &addr, sizeof(addr), 0);
+		addr = addr + offsets[i];
+	}
+	return addr;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	DWORD procId = GetProcId(L"Cheat Engine Tutorial Hack.exe");
+	uintptr_t moduleBase = GetModuleBaseAddress(procId, L"Cheat Engine Tutorial Hack.exe");
+	HANDLE hProcess = 0;
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+	if (hProcess && moduleBase && procId) {
+		uintptr_t dynamicPtrBaseAddr = moduleBase + 0x2015D0; // Why is the moduleBase always different?
+		std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicPtrBaseAddr << std::endl;
+
+		//uintptr_t test = 0;
+		//ReadProcessMemory(hProcess, (BYTE*)dynamicPtrBaseAddr, &test, sizeof(test), nullptr);
+		//std::cout << "test = " << "0x" << std::hex << test << std::endl;
+
+		std::vector<unsigned int> ammoOffsets = { 0x480, 0x0 };
+		uintptr_t ammoAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, ammoOffsets);
+		std::cout << "ammoAddr = " << "0x" << std::hex << ammoAddr << std::endl;
+
+		int ammoValue = 0;
+		ReadProcessMemory(hProcess, (LPVOID)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+		std::cout << "Curent ammo = " << std::dec << ammoValue << std::endl;
+
+		int newAmmo = 1337;
+		WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
+		ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+		std::cout << "New ammo = " << std::dec << ammoValue << std::endl;
+
+	}
+	getchar();
+	return 0;
 }
-
-// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
